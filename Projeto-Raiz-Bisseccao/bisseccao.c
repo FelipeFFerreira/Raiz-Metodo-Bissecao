58d9c1e489ab744bf86c0648ece2a96a0b95984e
Felipe Ferreira <felipe@verion.ind.br>
2020-12-22 00:23:38 -0300

    Organiza e add imgApresentacao

--- Projeto-Raiz-Bisseccao/bisseccao.c
+++ Projeto-Raiz-Bisseccao/bisseccao.c
@@ -25,7 +25,7 @@ void calcular_intervalo(bool op, double * x1, double * x2) {
      }
         else {
             _x1 =  0;
-            for(i = 0; imagem >= 0; i-- ) {
+            for (i = 0; imagem >= 0; i-- ) {
                 imagem = calcular_imagem(_x1 + i) * calcular_imagem(_x1 + i - 1);
                 printf("\nf(%.2lf) = %.2lf , f(%.2lf) = %.2lf\n", _x1 + i, calcular_imagem(_x1 + i), _x1 + i - 1, calcular_imagem (_x1 + i - 1));
         }
@@ -39,13 +39,13 @@ void calcular_raiz(bool op,double a, double b)
 {
     double xn = 0, sinal = 0, parada = 1;
     int cont = 0;
-    while(parada  >= 0.01){
+    while (parada  >= 0.01){
         cont++;
         parada = op == true ? (a - b) : (a * -1) - (b * -1);
         xn = ((a + b) / 2) ;
         printf("%d. [ a = %.3lf .:. b = %.3lf .:. xn = %.3lf ] f(a) = %.3lf .:. f(b) = %.3lf .:. f(xn) = %.3lf\t[fn*fn = %.3lf\t] .:. ", cont, b, a, xn, calcular_imagem(b), calcular_imagem(a), calcular_imagem(xn), calcular_imagem(a) * calcular_imagem(xn));
         sinal = calcular_imagem(a) * calcular_imagem(xn);
-        if(sinal < 0 ) b = xn;
+        if (sinal < 0 ) b = xn;
         else a = xn;
         printf("parada = %lf\n", parada);
     }
